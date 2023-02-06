492a85bdc348bdb0cd884a9ae12f0b26fa926720
nezuu2 <nezuu_pc2@yahoo.co.jp>
2023-02-06 17:11:35 +0900

    yui_devの内容をpush

---
+++ header&footer.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    
+    $('#footer_content').append('<!-- <div class="footer-logo"></div> -->',
+                                '<div class="footer-list">',
+                                '<ul id="footer"><!-- header&footer.jsで記載：一括編集の為 --></ul>',
+                                '<div id="auther"><a href="project-member.html" class="text-button">©nezuu2 & Projectメンバー</a><a href="project-member.html" class="text-button">Projectリーダー: 森田健太</a>',
+                                '<div id="social-media" style="position:relative;top:10px;font-size:17px;">',
+                                '<a href="https://twitter.com/share?url=https://nezuu2.github.io/YUI_web/" class="fab fa-twitter fa-lg"></a>',
+                                '<a href="https://instagram.com" class="fab fa-instagram fa-lg"></a>',
+                                '<a href="https://linevoom.line.me/" class="fab fa-line fa-lg text-button"></a>',
+                                '<a href="privacy-policy.html" class="text-button">プライバシーポリシー</a>',
+                                '</div>',
+                                '</div>',
+                                '</div>');
+
+    $('#header').append('<li><a href="project-member.html" class="menu tabs-button">YUIについて</a></li>',
+                        '<li><a href="#" class="menu tabs-button">創始者もりけん。</a></li>',
+                        '<li><a href="history.html" class="menu tabs-button">歴史</a></li>',
+                        '<li><a href="news.html" class="menu tabs-button">NEWS</a></li>',
+                        '<li><a href="contact-us.html" class="menu tabs-button">育成相談窓口</a></li>');
+
+{/* <li><a href="https://play.google.com/store/apps/details?id=com.websarva.wings.android.yui" class="menu tabs-button">アプリ</a></li>
+    <li><a href="counseling.html" class="menu tabs-button">カウンセリング</a></li>
+    <li><a href="javascript:alert('YouTubeアカウントは製作予定です。');" class="menu tabs-button black">YouTube</a></li>
+    <li><a href="https://twitter.com/Project_YUI816" class="menu tabs-button" target="_blank" rel="noopener noreferrer">Twitter</a></li>
+    <li><a href="https://twitter.com/kuromoriken" class="menu tabs-button" target="_blank" rel="noopener noreferrer">開発者のTwitter</a></li> */}
+    
+    $('#footer').append('<li class="footer-button"><a href="project-member.html">YUIについて</a></li>',
+                        '<li class="footer-button"><a href="#">創始者もりけん。</a></li>',
+                        '<li class="footer-button"><a href="history.html">歴史</a></li>',
+                        '<li class="footer-button"><a href="news.html">NEWS</a></li>',
+                        '<li class="footer-button"><a href="contact-us.html">育成相談窓口</a></li>');
+
+{/* <li class="footer-button"<a href="will.html" class="button">志</a></li>
+    <li class="footer-button"><a href="catch-copy.html" class="button">キャッチコピー</a></li>
+    <li class="footer-button"><a href="credo.html" class="button">クレド</a></li>
+    <li class="footer-button"><a href="project-member.html" class="button">Projectメンバー</a></li>
+    <li class="footer-button"><a href="history.html" class="button">歴史</a></li>
+    <li class="footer-button"><a href="contact-us.html" class="button">育成相談窓口</a></li>
+    <li class="footer-button"><a href="materials-used.html" class="button">使用した素材</a></li>
+    <li class="footer-button"><a href="privacy-policy.html" class="button">PrivacyPolicy</a></li> */}
+
+});
